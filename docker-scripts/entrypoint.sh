7696cadf03b6ff8c84d1353f043402830bdbd4d7
compilercomplied <8614035+compilercomplied@users.noreply.github.com>
2026-02-16 16:33:56 +0100

    treat github_token as non-mandatory

--- docker-scripts/entrypoint.sh
+++ docker-scripts/entrypoint.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 set -e
 
-if [ -z "$GITHUB_TOKEN" ]; then
-    echo "Error: GITHUB_TOKEN environment variable is not set."
-    exit 1
-fi
-
 if [ -z "$PULUMI_CONFIG_PASSPHRASE" ]; then
     echo "Error: PULUMI_CONFIG_PASSPHRASE environment variable is not set."
     exit 1
 fi
 
 
-git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
+
+# We can use variables that start with GITHUB_ in the pipeline.
+# While this variable should be absolutely mandatory, we can't enforce it if we
+# want E2E tests to work in the pipeline and gatekeep code quality.
+if [ "$GITHUB_TOKEN" ]; then
+	git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
+else
+	echo "GITHUB_TOKEN is not set, skipping git configuration"
+fi
 
 eval "$(mise activate bash)"
 
